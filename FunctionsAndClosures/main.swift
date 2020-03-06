ae63550872886d3a3308cc8d2cb095aefefef66c
skrall3119 <alexjanci@yahoo.com>
2020-03-06 15:02:00 -0600

    Initial Commit

---
+++ FunctionsAndClosures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FunctionsAndClosures
+//
+//  Created by Alex Janci on 3/6/20.
+//  Copyright © 2020 Alex Janci. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
